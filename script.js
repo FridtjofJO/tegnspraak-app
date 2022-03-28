3288766e61403726656d86dfead975acb0ed4dfb
FridtjofJO <fridtjof.johan@gmail.com>
2022-03-28 13:12:15 +0200

    endre "sannsynlighet" til "nøyaktighet"

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ async function predict() {
             let highest = finnHøyest(prediction);
             likhetsProsentValue = (likhetsProsentValue * highest.probability);
             console.log(likhetsProsentValue);
-            likhetsProsent.innerHTML = `Sannsynlighet: ${Math.floor(likhetsProsentValue*100)}%`;
+            likhetsProsent.innerHTML = `Nøyaktighet: ${Math.floor(likhetsProsentValue*100)}%`;
         }
         
     }        
